2f3309b4be0b01fc90d21b7566338a2ddb7f45d7
idle947 <idle947@gmail.com>
2022-02-02 16:22:56 +0900

    change db

--- heroku/resource.tf
+++ heroku/resource.tf
@@ -16,20 +16,8 @@ resource "heroku_addon" "prd_db" {
   name = "myapp-prd-db"
 }
 
-resource "heroku_addon" "dev_db" {
-  app  = heroku_app.my_app.name
-  plan = "heroku-postgresql:hobby-dev"
-  name = "myapp-dev-db"
-}
-
 resource "heroku_addon_attachment" "prd_database" {
   app_id   = heroku_app.my_app.id
   addon_id = heroku_addon.prd_db.id
   name     = "myapp_prd_db"
-}
-
-resource "heroku_addon_attachment" "dev_database" {
-  app_id   = heroku_app.my_app.id
-  addon_id = heroku_addon.dev_db.id
-  name     = "myapp_dev_db"
 }
